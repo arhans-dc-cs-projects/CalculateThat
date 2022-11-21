1d832b7babe966f5e060957a1cdddcc9e13f0cd9
Arhan <arhanh1234@gmail.com>
2022-11-21 08:27:54 +0300

    Add files via upload

---
+++ Challenge - Calculate That!.py
@@ -0,0 +1,18 @@
+name = input("Enter your name: ")
+print(name)
+
+number = int(input("Enter a number: "))
+print(number)
+
+namelen = len(name)
+print(number+namelen)
+
+print(name*number)
+
+print(number/namelen)
+
+print(number%namelen)
+
+print(namelen*2-number)
+
+print(number**namelen)
